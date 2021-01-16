2a768b8e7ac4b244a223907cf3e70ae018f5e29b
AnikethDandu <aniketh.dandu@icloud.com>
2021-01-16 13:18:16 -0500

    Add Rotten Tomatoes genres, genre and year argument, and genre check after optional input entered

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 import argparse
+from bs4 import BeautifulSoup
+import urllib
+
+MOVIE_GENRES = [
+    'Action & Adventure',
+    'Animation',
+    'Art house & International',
+    'Classics',
+    'Comedy',
+    'Drama',
+    'Kids & Family',
+    'Musical & Performing Arts',
+    'Mystery & Suspense',
+    'Romance',
+    'Science Fiction & Fantasy',
+    'Special Interest',
+    'Sports & Fitness',
+    'Television',
+    'Western',
+]
 
 parser = argparse.ArgumentParser(description='Return top movies from Rotten Tomatoes site')
-parser.add_argument('--n', type=int, nargs='?', const='10', default='10')
+parser.add_argument('-l', '--length', type=int, nargs='?', default=10)
+parser.add_argument('-g', '--genre', type=str, nargs='?', default=None,
+                    help=f'Movie genres: {", ".join(list([movie for movie in MOVIE_GENRES]))}')
+parser.add_argument('-y', '--year', type=int, nargs='?', default=2020)
 args = parser.parse_args()
+
+if args.genre not in MOVIE_GENRES and args.genre is not None:
+    print(f'{args.genre} is not a valid genre. '
+          f'Please select one of the following:\n'
+          f'{", ".join(list([movie for movie in MOVIE_GENRES]))}')
+    exit()
